951754857ca61107a1d1e85981e34636c62ec90a
Steve Purcell <steve@sanityinc.com>
2011-11-11 12:29:49 +0000

    With a prefix-arg, prompt for the symbol to which to jump.

--- elisp-slime-nav.el
+++ elisp-slime-nav.el
@@ -3,16 +3,25 @@
 ;; Author: Steve Purcell <steve@sanityinc.com>
 ;; Keywords: navigation slime elisp emacs-lisp
 ;; URL: https://github.com/purcell/elisp-slime-nav
-;; Version: 0.1
+;; Version: DEV
 ;;
 ;;; Commentary
 ;;
 ;; This package provides Slime's convenient "M-." and "M-," navigation
 ;; in `emacs-lisp-mode'.
 ;;
+;; Additionally, C-u M-. will prompt for the symbol to which to jump.
+;;
 ;; Usage:
+;;
 ;;   (require 'elisp-slime-nav)
 ;;   (add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+;;
+;; Known issues:
+;;
+;;   When navigating into Emacs' C source, "M-," will not be bound to
+;;   the same command, but "M-*" will typically do the trick.
+;;
 ;;; Code
 (defvar elisp-slime-nav-mode-map (make-keymap))
 
@@ -23,25 +32,45 @@
 
 (require 'etags)
 
+
+(defun elisp-slime-nav--all-navigable-symbol-names ()
+  "Return a list of strings for the symbols to which navigation is possible."
+  (loop for x being the symbols
+        if (or (fboundp x) (boundp x) (symbol-plist x) (facep x))
+        collect (symbol-name x)))
+
 ;;;###autoload
-(defun elisp-slime-nav-find-elisp-thing-at-point ()
-  "Jump to the elisp thing at point, whether it's a function,
-variable, library or face."
-  (interactive)
-  (let ((sym (symbol-at-point)))
-    (when sym
+(defun elisp-slime-nav-find-elisp-thing-at-point (sym-name)
+  "Jump to the elisp thing at point, be it a function,variable, library or face.
+
+With a prefix arg, prompt for the symbol to jump to.
+
+Argument SYM-NAME thing to find."
+  (interactive
+   (list
+    (let* ((sym-at-point (symbol-at-point))
+           (at-point (and sym-at-point (symbol-name sym-at-point))))
+      (if current-prefix-arg
+          (completing-read "Symbol: "
+                           (elisp-slime-nav--all-navigable-symbol-names)
+                           nil t at-point)
+        at-point))))
+  (when sym-name
+    (let ((sym (intern sym-name)))
+      (message "search for %s" (pp-to-string sym))
       (ring-insert find-tag-marker-ring (point-marker))
       (cond
        ((fboundp sym) (find-function sym))
        ((boundp sym) (find-variable sym))
-       ((or (featurep sym) (locate-library (symbol-name sym)))
-        (find-library (symbol-name sym)))
+       ((or (featurep sym) (locate-library sym-name))
+        (find-library sym-name))
        ((facep sym)
         (find-face-definition sym))
        (:else
         (progn
           (pop-tag-mark)
           (error "Don't know how to find '%s'" sym)))))))
+
 
 (define-key elisp-slime-nav-mode-map (kbd "M-.") 'elisp-slime-nav-find-elisp-thing-at-point)
 (define-key elisp-slime-nav-mode-map (kbd "M-,") 'pop-tag-mark)
